60a215a839a7c65748f8465354916481716d5996
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 13:13:38 +0300

    Max number search

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int a = new Random().Next(0,10);
+int b = 6;
+int c = 1;
+int d = 7;
+int e = 9;
+
+int maxNum = a;
+
+if (a>maxNum) {maxNum = a;}
+if (b>maxNum) {maxNum = b;}
+if (c>maxNum) {maxNum = c;}
+if (d>maxNum) {maxNum = d;}
+if (e>maxNum) {maxNum = e;}
+
+WriteLine(a);
+Write("Max number is ");
+WriteLine (maxNum);
