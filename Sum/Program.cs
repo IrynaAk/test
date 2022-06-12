5c725620d9daa12e9305a5a4867d4cac4ac1b5e2
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 13:04:11 +0300

    Division was added to Sum folder

---
+++ Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int number1 = new Random().Next(0,100);
+int number2 = new Random().Next(0,100);
+
+int result = number1+number2;
+
+WriteLine(number1);
+WriteLine(number2);
+WriteLine(result);
+
+//Division
+
+WriteLine("Devision:");
+int num3 = new Random().Next(0,100);
+int num4 = new Random().Next(1,100);
+
+int divResult = num3/num4;
+
+WriteLine(num3);
+WriteLine(num4);
+WriteLine(divResult);
