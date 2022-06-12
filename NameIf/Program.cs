ed3b4d0fe6141c2e6d51f6f663a9fa2d2d7e15cf
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 12:38:45 +0300

    Hello User - Codition If

---
+++ NameIf/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Please type your name");
+
+string name = ReadLine();
+
+if (name.ToLower () == "iryna")
+{
+WriteLine("Hey, Iryna! Glad to see you!");
+}
+else
+{
+    Write("Hello, ");
+    WriteLine (name);
+    }
